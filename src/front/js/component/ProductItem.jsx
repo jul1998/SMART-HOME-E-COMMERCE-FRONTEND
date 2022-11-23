3c0cfd59f54d1e56a304a26911ffa4cd3708da16
jul1998 <jguevara321@gmail.com>
2022-11-23 20:03:28 +0000

    product page template and new footer option finied

--- src/front/js/component/ProductItem.jsx
+++ src/front/js/component/ProductItem.jsx
@@ -1,33 +1,25 @@
 import { Link } from "react-router-dom";
 import React, { useState, useContext, useEffect } from "react";
-import "../../styles/login.css";
+import "../../styles/products.css";
 import { Context } from "../store/appContext";
 
 function ProductItem({product}) {
-  const { store, actions } = useContext(Context);
-  const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    async function fetch() {
-      let response = await actions.genericFetch("products_list");
-      let jsonResponse = await response.json();
-      setProducts(jsonResponse);
-    }
-
-    fetch();
-  }, []);
-
-  console.log(products);
+  const priceDisplay = product.precio.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
 
   return (
-    <div>
+    <div className="product_list_container">
     
-        <div class="col">
+        <div className="col">
             <div className="card h-100">
-              <img src="..." className="card-img-top" alt="..."/>
+              <img className="product_image" src="https://img.freepik.com/free-photo/medium-shot-man-wearing-vr-glasses_23-2149126949.jpg?w=1060&t=st=1669229881~exp=1669230481~hmac=8d0657c3f598a067a170b07757c757f2d011f31316863f13c0b677472f28fe1d" alt="..."/>
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a short card.</p>
+                <h5 className="card-title">{product.name}</h5>
+                <p class="card-text">Estado: {product.estado}</p>
+                <p class="card-text">Precio: {priceDisplay}</p>
               </div>
             </div>
           </div>
