be9cca71ef37ba404198b2795305ea084a594473
jul1998 <jguevara321@gmail.com>
2022-11-24 03:28:29 +0000

    fixed admin bug

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -9,7 +9,6 @@ import { Signup } from "./pages/signup.jsx"; //Component to register user
 import { Login } from "./pages/Login.jsx";
 import { Products } from "./pages/ProductPage.jsx"
 import { LoginAdmin } from "./pages/LoginAdmin.jsx"
-import { SignupAdmin } from "./pages/signupAdmin.jsx";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -34,7 +33,7 @@ const Layout = () => {
                         <Route element={<Login />} path="/login" />
                         <Route element={<Products />} path="/products" />
                         <Route element={<LoginAdmin />} path="/LoginAdmin" />
-                        <Route element={<SignupAdmin />} path="/signupAdmin" />
+            
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
