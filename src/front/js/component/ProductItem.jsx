443c700d7c1d8ac98e92e87f8e3d4e37af1dbfb1
SebastianJimenez2510 <sebastian.jimenezgarro@gmail.com>
2022-12-10 05:24:00 +0000

    Avance de las cards

--- src/front/js/component/ProductItem.jsx
+++ src/front/js/component/ProductItem.jsx
@@ -31,9 +31,7 @@ function ProductItem({product}) {
 
 
   return (
-    <div className="product_list_container">
-    
-        <div className="col">
+      
             <div className="card h-100">
               <img className="product_image" src="https://img.freepik.com/free-photo/medium-shot-man-wearing-vr-glasses_23-2149126949.jpg?w=1060&t=st=1669229881~exp=1669230481~hmac=8d0657c3f598a067a170b07757c757f2d011f31316863f13c0b677472f28fe1d" alt="..."/>
               <div className="card-body">
@@ -44,11 +42,7 @@ function ProductItem({product}) {
                 {showFavButton}
               </div>
             </div>
-          </div>
-        
 
-      
-    </div>
   );
 }
 
--- src/front/js/pages/ProductPage/ProductPage.jsx
+++ src/front/js/pages/ProductPage/ProductPage.jsx
@@ -11,8 +11,11 @@ function Products() {
   useEffect(() => {
     async function fetch() {
       let response = await actions.genericFetch("products_list");
+      console.log(response)
       let jsonResponse = await response.json();
+      console.log(jsonResponse)
       setProducts(jsonResponse);
+      
     }
 
     fetch();
@@ -24,9 +27,9 @@ function Products() {
 
   return (
     <div>
-      <div className="container text-center">
+      <div className="container-fluid text-center">
         <div className="row">
-          <div className="col">{displayProducts}</div>
+          <div className="col-12 col-md-4">{displayProducts}</div>
         </div>
       </div>
     </div>
